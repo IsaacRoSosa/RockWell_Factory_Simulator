02bf5ffd6e2aaf895c94e9e3401dc2fa06b3fe06
Isaac Rojas <isaacrojassosa@gmail.com>
2024-06-10 18:54:53 -0600

    Game Implemented to the User

--- rockwell-automation-simulator/src/components/UserGameView.jsx
+++ rockwell-automation-simulator/src/components/UserGameView.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import styles from '@/styles/UserGameView.module.css'
 
 const UserGameView = () => {
-    return (
-        <div className={styles.section}>
-        <div className={styles.gameContainer}></div>
-    
-      </div> 
-    )
+  return (
+    <div className={styles.section}>
+      <div className={styles.gameContainer}>
+        <iframe 
+          src="/Build/index.html" 
+          width="100%" 
+          height="100%" 
+          frameBorder="0" 
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
 }
 
-export default UserGameView
+export default UserGameView  
